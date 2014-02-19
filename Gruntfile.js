25d1f62e03410531f1f17dda5d765df3d9f78977
idesota <idesota@gmail.com>
2014-02-19 14:37:01 +0900

    add HTMLmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,74 @@
 /*
-ターミナルでプラグン読み込み
+【構造】
+developで開発、releaseでminify
+*/
+
+/*
+【スタートの際：プラグン】
+
+●1つづつ読み込み
 ex.
 npm install grunt-contrib-watch grunt-contrib-cssmin --save-dev
 
+●一度に読み込み
 package.jsonのdevDependenciesにプラグインなどの情報が書かれていれば
 npm install
 でOK
-npm install grunt-contrib-uglify --save-dev
 */
 
 module.exports = function(grunt) {
   grunt.initConfig({
   	
-  	pkg: grunt.file.readJSON('package.json')
-  	
-  	, //JOINT
+  	// package.jsonの定義
+  	pkg: grunt.file.readJSON('package.json'),
   
   	//CSS min
   	cssmin: {
       compress: {
         files: {
-          'asset/min.css': ['asset/css/reset.css', 'asset/css/style.css']
+          'release/asset/css/min.css': ['develop/asset/css/reset.css', 'develop/asset/css/style.css']
         }
       }
     },
     watch: {
-      files: ['asset/css/*.css'],
+      files: ['develop/asset/css/*.css'],
       tasks: ['cssmin']
-    }
-    //**
-    
-    , //JOINT
+    },
     
     //JS min
     uglify: {
     my_target: {
       files: {
-        'asset/js/script.min.js': ['asset/js/script.js', 'asset/js/script.js']
+        'release/asset/js/script.min.js': ['develop/asset/js/script.js', 'develop/asset/js/script.js']
       	}
     	}
- 	 }
+ 	 },
+ 	 
+ 	 //HTML min
+ 	 htmlmin: {
+      all: {
+        options: {
+          removeComments: true,
+          removeCommentsFromCDATA: true,
+          removeCDATASectionsFromCDATA: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          removeOptionalTags: true
+        },
+        expand: true,
+        cwd: 'develop/',
+        src: ['**/*.html'],
+        dest: 'release/'
+      }
+    },
+    
+    // clean 不要ファイルを削除
+    clean: {
+      // 最初にreleaseディレクトリ内を削除
+      deleteReleaseDir: {
+        src: 'release/'
+      }
+    }
   
   
   });
@@ -47,10 +76,11 @@ module.exports = function(grunt) {
   //タスク読み込み
   grunt.loadNpmTasks('grunt-contrib-cssmin'); //CSSmin
   grunt.loadNpmTasks('grunt-contrib-uglify');  //JSmin
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');  //htmlmin
   grunt.loadNpmTasks('grunt-contrib-watch'); //ウォッチ
   
   //defaultを設定すると"grunt"だけで下記実行
-  grunt.registerTask('default', ['cssmin', 'uglify', 'watch']);
+  grunt.registerTask('default', ['cssmin', 'uglify', 'htmlmin', 'watch']);
 };
 
 /*
